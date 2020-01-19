8d0663be605703ee81c41904aaa3227971c0b2bd
Tian Qin <qintian29@gmail.com>
2020-01-19 21:18:58 +0800

    tian

--- public/js/events.js
+++ public/js/events.js
@@ -128,35 +128,6 @@ $(document).ready(function() {
     newEventCardButtons.append(joinEventButton).append(exitEventButton);
   }
 
-  // A function for handling what happens when the form to create a new post is submitted
-  function handleFormSubmit(event) {
-    event.preventDefault();
-    // Wont submit the post if we are missing a body, title, or author
-    if (
-      !titleInput.val().trim() ||
-      !bodyInput.val().trim() ||
-      !authorSelect.val()
-    ) {
-      return;
-    }
-    // Constructing a newPost object to hand to the database
-    var newPost = {
-      title: titleInput.val().trim(),
-      body: bodyInput.val().trim(),
-      AuthorId: authorSelect.val()
-    };
-
-    // If we're updating a post run updatePost to update a post
-    // Otherwise run submitPost to create a whole new post
-    if (updating) {
-      newPost.id = postId;
-      updatePost(newPost);
-    } else {
-      submitPost(newPost);
-    }
-  }
-
-  // Submits a new post and brings user to blog page upon completion
   function submitPost(post) {
     $.post("/api/posts", post, function() {
       window.location.href = "/blog";
@@ -164,31 +135,6 @@ $(document).ready(function() {
   }
 
   // Gets post data for the current post if we're editing, or if we're adding to an author's existing posts
-  function getPostData(id, type) {
-    var queryUrl;
-    switch (type) {
-      case "post":
-        queryUrl = "/api/posts/" + id;
-        break;
-      case "author":
-        queryUrl = "/api/authors/" + id;
-        break;
-      default:
-        return;
-    }
-    $.get(queryUrl, function(data) {
-      if (data) {
-        console.log(data.AuthorId || data.id);
-        // If this post exists, prefill our cms forms with its data
-        titleInput.val(data.title);
-        bodyInput.val(data.body);
-        authorId = data.AuthorId || data.id;
-        // If we have a post with this id, set a flag for us to know to update the post
-        // when we hit submit
-        updating = true;
-      }
-    });
-  }
 
   // A function to get Authors and then render our list of Authors
   function getAuthors() {
@@ -212,22 +158,14 @@ $(document).ready(function() {
     authorSelect.val(authorId);
   }
 
-  // Creates the author options in the dropdown
-  function createAuthorRow(author) {
-    var listOption = $("<option>");
-    listOption.attr("value", author.id);
-    listOption.text(author.name);
-    return listOption;
-  }
-
   // Update a given post, bring user to the blog page when done
-  function updatePost(post) {
-    $.ajax({
-      method: "PUT",
-      url: "/api/posts",
-      data: post
-    }).then(function() {
-      window.location.href = "/blog";
-    });
-  }
+  // function updatePost(post) {
+  //   $.ajax({
+  //     method: "PUT",
+  //     url: "/api/posts",
+  //     data: post
+  //   }).then(function() {
+  //     window.location.href = "/blog";
+  //   });
+  // }
 });
