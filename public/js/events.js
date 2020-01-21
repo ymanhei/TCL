c21fd1903be645a7099c4da38567c5d3263e188f
Lachlan <lachlanrjones@gmail.com>
2020-01-21 19:39:46 +0800

    shing's changes (#9)

--- public/js/events.js
+++ public/js/events.js
@@ -1,152 +1,21 @@
 $(document).ready(function() {
-/*
-THIS IS TIAN'S VERSION CAN YOU PLEASE HAVE A LOOK AS DEALING WITH THE NEXT THREE BUTTONS CAN BE BUILT UPON THESE
-
-  var url = window.location.search;
-
-  var dataDetail;
-  var eventsResult;
-  // var updating = false;
-  var eventsContainer = $("#events-container");
-
-  if (url.indexOf("?date=") !== -1) {
-    dataDetail = url.split("=")[1];
-    getEventsData(dataDetail, "date");
-  } else if (url.indexOf("?location=") !== -1) {
-    dataDetail = url.split("=")[1];
-    getEventsData(dataDetail, "location");
-  } else if (url.indexOf("?category=") !== -1) {
-    dataDetail = url.split("=")[1];
-    getEventsData(dataDetail, "category");
-  } else {
-    getEventsData();
-  }
-
-  function getEventsData(data, type) {
-    var queryUrl;
-    var typeDetail;
-    dataDetail = data || "";
-    typeDetail = type || "";
-
-    switch (typeDetail) {
-      case "date":
-        queryUrl = "/?date=" + dataDetail;
-        break;
-      case "location":
-        queryUrl = "/?location=" + dataDetail;
-        break;
-      case "category":
-        queryUrl = "/?category=" + dataDetail;
-        break;
-      case "":
-        queryUrl = "";
-        break;
-    }
-
-    $.get("/api/events" + queryUrl, function(data) {
-      console.log("Events", data);
-      eventsResult = data;
-      if (!eventsResult || !eventsResult.lengths) {
-        displayEmpty(type);
-      } else {
-        initializeEventRows();
-      }
-    });
-  }
-
-  function displayEmpty(type) {
-    var partial = "";
-    if (type) {
-      partial = "For This " + type;
-    }
-    var noEventsMessage = $("<h2>");
-    noEventsMessage.css({
-      "text-align": "center",
-      "margin-top": "50px",
-      "font-family": "'Oswald', sans-serif"
-    });
-    noEventsMessage.html(
-      `Unfortunately There Are No Events Found ${partial}, Please Try Other Searches From <a href="/">Here</a>.`
-    );
-    eventsContainer.append(noEventsMessage);
-  }
-
-  function initializeEventRows() {
-    eventsContainer.empty();
-
-    var eventsToAdd = [];
-
-    for (let i = 0; i < eventsResult.length; i++) {
-      eventsToAdd.push(createEachEvent(eventsResult[i]));
-    }
-
-    eventsContainer.append(eventsToAdda);
-  }
-
-  function createEachEvent(event) {
-    var eventsContainer = $("#events-container");
-    var newEventBox = $("<div>");
-    newEventBox.css("display", "inline-block !important");
-    newEventBox.addClass("event-box col-12 col-md-6 col-lg-4 col-xl-3");
-    var newEventCard = $("<div>");
-    newEventCard.addClass("card");
-
-    var newEventCardHeader = $("<div>");
-    newEventCardHeader.addClass("card-header");
-    newEventCardHeader.text(event.activity);
-    var newEventCardImage = $("<img>");
-    newEventCardImage.addClass("card-img");
-    newEventCardImage.attr("src", event.imageUrl);
-    var newEventCardDescription = $("<div>");
-    newEventCardDescription.addClass("card-body");
-    var descriptionText = $("<p>");
-    descriptionText.addClass("card-text");
-    descriptionText.text(event.description);
-    var newEventCardDetailList = $("<ul>");
-    newEventCardDetailList.addClass("list-group list-group-flush");
-    var newEventCardDate = $("<li>");
-    newEventCardDate.addClass("list-group-item");
-    newEventCardDate.text(event.date);
-    var newEventCardLocation = $("<li>");
-    newEventCardLocation.addClass("list-group-item");
-    newEventCardLocation.text(event.location);
-    var newEventCardCategory = $("<li>");
-    newEventCardCategory.addClass("list-group-item");
-    newEventCardCategory.text(event.category);
-    var newEventCardGroupSize = $("<li>");
-    newEventCardGroupSize.addClass("list-group-item");
-    var newEventCardButtons = $("<div>");
-    newEventCardButtons.addClass("card-body");
-    var joinEventButton = $("<button>");
-    joinEventButton.addClass("btn btn-primary btn-lg").text("JOIN");
-
-    var exitEventButton = $("<button>");
-    exitEventButton.addClass("btn btn-warning btn-lg").text("EXIT");
-
-    newEventBox.append(newEventCard);
-    newEventCard
-      .append(newEventCardHeader)
-      .append(newEventCardImage)
-      .append(newEventCardDescription);
-    newEventCardDescription.append(descriptionText);
-    newEventCard.append(newEventCardDetailList);
-    newEventCardDetailList
-      .append(newEventCardLocation)
-      .append(newEventCardCategory)
-      .append(newEventCardGroupSize);
-    newEventCard.append(newEventCardButtons);
-    newEventCardButtons.append(joinEventButton).append(exitEventButton);
-    return newEventBox;
-  }
-
-*/
-  
-  
-  // BELOW ARE LACHLAN'S VERSION WHICH I HAVE NOT TOUCHED//
-  
   var blogContainer = $(".blog-container");
 
-  $.get("/api/events", function(data) {
+  var url = window.location.search;
+  console.log("URL:"+url);
+  var searchtype;
+  var value;
+  if (url.indexOf("?category=") !== -1 || url.indexOf("?location=") !== -1 || url.indexOf("?date=") !== -1) {
+    searchtype = url.split("=")[0];
+    value = url.split("=")[1];
+    getPosts(searchtype.substr(1),value);
+  }
+  // If there's no authorId we just get all posts as usual
+  else {
+    getPosts();
+  }
+
+  /* $.get("/api/events", function(data) {
     console.log("Events", data);
     posts = data;
 
@@ -176,7 +45,24 @@ THIS IS TIAN'S VERSION CAN YOU PLEASE HAVE A LOOK AS DEALING WITH THE NEXT THREE
     } else {
       initializeRows();
     }
-  });
+  }); */
+
+  function getPosts(st,val) {
+    searchtype = st || "";
+    value = val || "";
+    if (searchtype) {
+      searchtype = "/" + searchtype + "/" ;
+    }
+    $.get("/api/events" + searchtype + value, function(data) {
+      console.log("searchtype", searchtype);
+      console.log("value", value);
+      console.log("Posts", data);
+      posts = data;
+      
+        initializeRows();
+      
+    });
+  } 
 
   // InitializeRows handles appending all of our constructed post HTML inside blogContainer
   function initializeRows() {
@@ -235,5 +121,4 @@ THIS IS TIAN'S VERSION CAN YOU PLEASE HAVE A LOOK AS DEALING WITH THE NEXT THREE
     newPostCard.data("post", post);
     return newPostCard;
   }
-
 });
