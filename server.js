51f39dcafb93f8b67406b2c10bf62de00a93e3cf
Lachlan <lachlanrjones@gmail.com>
2020-01-21 20:49:33 +0800

    fixed admin (#12)
    * shing's changes
    
    * fixed admin

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ require("./routes/joinersactivities-api-routes.js")(app);
 
 // Syncing our sequelize models and then starting our Express app
 // =============================================================
-db.sequelize.sync({ force: true }).then(function() {
+db.sequelize.sync().then(function() {
   app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
   });
