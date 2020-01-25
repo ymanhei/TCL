66c6dafa72b57d09ec7ec06c28f25925c86f22bd
Tian Qin <qintian29@gmail.com>
2020-01-25 13:23:32 +0800

    f

--- models/events.js
+++ models/events.js
@@ -32,19 +32,7 @@ module.exports = function(sequelize, DataTypes) {
     }
   });
 
-  /* Event.associate = function(models) {
-    // We're saying that a Event should belong to an user
-    // A Event can't be created without an user due to the foreign key constraint
-    Event.belongsTo(models.User, {
-      foreignKey: {
-        allowNull: false
-      }
-    });
-  }; */
-
   Event.associate = function(models) {
-    // We're saying that a Event should belong to an initiator
-    // A Event can't be created without an initiator due to the foreign key constraint
     Event.belongsToMany(models.User, {
       through: "JoinersActivities"
     });
